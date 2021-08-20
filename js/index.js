5b2b78d8129bc3d3c9f00ad14ac687c8f6d09b0b
Stefany Repetcki <stefany.repetcki@mirumagency.com>
2021-08-20 17:28:36 -0300

    feat: implementation LP

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  // Activate Carousel
+  $('.carousel').carousel();
+  $('[data-toggle="tooltip"]').tooltip({ html: true, placement: 'bottom' });
+
+  /** This section is only needed once per page if manually copying **/
+  if (typeof MauticSDKLoaded == 'undefined') {
+    var MauticSDKLoaded = true;
+
+    var head = document.getElementsByTagName('head')[0];
+
+    var script = document.createElement('script');
+
+    script.type = 'text/javascript';
+
+    script.src = 'https://mautic.azmina.com.br/media/js/mautic-form.js';
+
+    script.onload = function () {
+      MauticSDK.onLoad();
+    };
+
+    head.appendChild(script);
+
+    var MauticDomain = 'https://mautic.azmina.com.br';
+
+    var MauticLang = {
+      submittingMessage: 'Enviando...'
+    };
+  }
+});
