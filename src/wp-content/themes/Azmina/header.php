b04bf698e6b85413866453da1230aebb17e093ae
Laerte Oliveira <laerte.oliveira@mirumagency.com>
2021-10-06 18:29:37 -0300

    Bug fixing and removing unused plugins

--- src/wp-content/themes/Azmina/header.php
+++ src/wp-content/themes/Azmina/header.php
@@ -4,21 +4,19 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title></title>
+    <meta name=”description” content="<?php the_excerpt(); ?>">
+    <title><?php echo get_the_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/dist/css/style.min.css" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/dist/css/main.css" />
-    <link
+    <!-- <link
       rel="stylesheet"
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossorigin="anonymous"
-    />
+    /> -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
-      rel="stylesheet"
-    />
+    <link  href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"/>
   </head>
   <body>
     <?php $bgDesk = get_field('banner-imagem-de-fundo-desk'); ?>
