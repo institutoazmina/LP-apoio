c4bd24f7d8743e6b4590702259a8d9a2f9fc2710
sobral <robson@oficiodesign.com>
2021-11-04 13:09:11 -0300

    Kill gremlins

--- src/wp-content/themes/Azmina/header.php
+++ src/wp-content/themes/Azmina/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name=”description” content="<?php the_excerpt(); ?>">
+    <meta name="description" content="<?php the_excerpt(); ?>">
     <title><?php echo get_the_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/dist/css/style.min.css?v=<?php echo rand();?>" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/dist/css/main.css?v=<?php echo rand();?>" />
